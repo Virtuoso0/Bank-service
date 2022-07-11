8f59f5759d9bd1f57454a245675779e9feab3860
Patryk Piecuch <patrykpiecuch@protonmail.com>
2022-07-11 22:44:50 +0200

    sortowanie i paginacja

--- src/main/java/org/kaczucha/repository/ClientSpringJpaRepository.java
+++ src/main/java/org/kaczucha/repository/ClientSpringJpaRepository.java
@@ -1,6 +1,8 @@
 package org.kaczucha.repository;
 
 import org.kaczucha.repository.entity.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,6 +20,8 @@ public interface ClientSpringJpaRepository extends JpaRepository<Client, Long> {
     Client findByEmail(@Param("email") String email);
 
     List<Client> findByName(String name);
+
+    Page<Client> findByName(String name, Pageable pageable);
 
     void deleteByEmail(String email);
 }
